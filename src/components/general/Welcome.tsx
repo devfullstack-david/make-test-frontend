2ccd5c3c233f0d56fe06c90e54b6a370a9c01427
devfullstack-david <dev.fullstack.david@gmail.com>
2025-06-14 01:31:45 -0300

    Finalize home page and initial screen

--- src/components/general/Welcome.tsx
+++ src/components/general/Welcome.tsx
@@ -6,17 +6,33 @@ const Welcome = () => {
     <div className='welcome-container'>
       <div className='welcome-container__create'>
         <div className='welcome-container__text'>
-          CRIE <br />SUA<br /> AVALIAÇÃO
+          CRIE 
+          <br className='breakline'/> SUA 
+          <br className='breakline'/> AVALIAÇÃO
         </div>
 
-        <Image width={1000} height={400} src="/images/arrow-left.png" alt="arrow-left" />
+        <Image 
+          width={1000} 
+          height={300} 
+          src="/images/arrow-left.png" 
+          alt="arrow-left"
+          className='welcome-container__image' 
+        />
       </div>
 
       <div className='welcome-container__make'>
-        <Image width={1000} height={400} src="/images/arrow-right.png" alt="arrow-left" />
+        <Image 
+          width={1000} 
+          height={300} 
+          src="/images/arrow-right.png" 
+          alt="arrow-left" 
+          className='welcome-container__image' 
+        />
 
         <div className='welcome-container__text'>
-          CRIE <br />SUA<br /> AVALIAÇÃO
+          FAÇA 
+          <br className='breakline'/> UMA 
+          <br className='breakline'/> AVALIAÇÃO
         </div>
       </div>
     </div>
