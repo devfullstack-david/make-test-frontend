fbaee8114f5657845d4fe4bb34074e68c94652a0
devfullstack-david <dev.fullstack.david@gmail.com>
2025-06-20 04:12:29 -0300

    Refatorando página de bem vindo

--- src/components/general/Welcome.tsx
+++ src/components/general/Welcome.tsx
@@ -1,40 +1,50 @@
+import { welcomeSx } from '@/styles/components/welcome';
 import '@/styles/components/welcome.css';
-import Image from 'next/image';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
 
 const Welcome = () => {
+  const [email, setEmail] = useState<string>("");
   return (
     <div className='welcome-container'>
-      <div className='welcome-container__create'>
-        <div className='welcome-container__text'>
-          CRIE 
-          <br className='breakline'/> SUA 
-          <br className='breakline'/> AVALIAÇÃO
-        </div>
+      <Typography 
+        variant='h2' 
+        textAlign={"center"}
+        color='white'
+        sx={{ mt: 5 }}
+      >
+          Crie/faça uma <span className='primary_text'>avaliação</span>
+      </Typography>
 
-        <Image 
-          width={1000} 
-          height={300} 
-          src="/images/arrow-left.png" 
-          alt="arrow-left"
-          className='welcome-container__image' 
+      <Typography
+        textAlign={'center'}
+        color='#cfcfcf'
+        sx={{ mt: 5 }}
+      >
+        Make Test é uma plataforma 100% online para criação 
+        e realização de avaliações,<br /> 
+        feita para substituir os tradicionais 
+        testes em papel por uma <br />experiência 
+        digital poderosa, intuitiva e acessível.
+      </Typography>
+
+      <Box
+        display={'flex'}
+        alignContent={'center'}
+        justifyContent={'center'}
+        sx={{ mt: 5 }}
+      >
+        <TextField 
+          label="Digite seu email"
+          value={email}
+          sx={welcomeSx}
+          onChange={(e) => setEmail(e.target.value)}
         />
-      </div>
-
-      <div className='welcome-container__make'>
-        <Image 
-          width={1000} 
-          height={300} 
-          src="/images/arrow-right.png" 
-          alt="arrow-left" 
-          className='welcome-container__image' 
-        />
-
-        <div className='welcome-container__text'>
-          FAÇA 
-          <br className='breakline'/> UMA 
-          <br className='breakline'/> AVALIAÇÃO
-        </div>
-      </div>
+        <Button>Começar</Button>
+      </Box>
     </div>
   )
 }
---
+++ src/styles/components/welcome.ts
@@ -0,0 +1,22 @@
+export const welcomeSx = {
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'white', 
+      },
+      '&:hover fieldset': {
+        borderColor: 'white', 
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#00A648',
+      },
+    },
+    '& label': {
+      color: 'white',
+    },
+    '& label.Mui-focused': {
+      color: '#00A648',
+    },
+    input: {
+      color: 'white',
+    },
+  };
